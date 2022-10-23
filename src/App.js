36de2f4de54af26da2c107f41c2d9e9c42e33199
sholabioku <bilushonline@gmail.com>
2022-10-23 07:03:55 +0100

    Add route for success, cancel and store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import NavbarComponent from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Store from './pages/Store';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
 
 function App() {
   return (
     <Container>
       <NavbarComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path='success' element={<Success />} />
+          <Route path='cancel' element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
