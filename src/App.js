8541568611b54ca9bffea3720232c40b7c012dd5
sholabioku <bilushonline@gmail.com>
2022-10-25 07:42:58 +0100

    Add functionality for cart context

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Store from './pages/Store';
 import Success from './pages/Success';
 import Cancel from './pages/Cancel';
+import CartProvider from './CartContext';
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Store />} />
-          <Route path='success' element={<Success />} />
-          <Route path='cancel' element={<Cancel />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <CartProvider>
+      <Container>
+        <NavbarComponent />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store />} />
+            <Route path='success' element={<Success />} />
+            <Route path='cancel' element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
