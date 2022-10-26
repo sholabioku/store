62abcf8450b657ee6340fd3cf992c4c8534e2f8e
sholabioku <bilushonline@gmail.com>
2022-10-26 09:14:34 +0100

    Add functionality for stripe

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,6 +15,22 @@ const NavbarComponent = () => {
     0
   );
 
+  const checkout = async () => {
+    await fetch('http://localhost:4000/checkout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ items: cart.items }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        if (response.url) {
+          window.location.assign(response.url);
+        }
+      });
+  };
+
   return (
     <>
       <Navbar>
@@ -40,7 +56,9 @@ const NavbarComponent = () => {
                 ></CartProduct>
               ))}
               <h1>Total: ${cart.getTotalCost().toFixed(2)}</h1>
-              <Button variant='success'>Purchase items</Button>
+              <Button variant='success' onClick={checkout}>
+                Purchase items
+              </Button>
             </>
           ) : (
             <h1>There are no items in your cart</h1>
